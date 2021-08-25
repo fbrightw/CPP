e08f29a738fa9130c920db140d1be23b67c739f2
fbrightw <81295000+fbrightw@users.noreply.github.com>
2021-08-25 17:30:44 +0300

    Add files via upload

---
+++ module07/ex00/whatever.hpp
@@ -0,0 +1,29 @@
+#ifndef WHATEVER_HPP
+#define WHATEVER_HPP
+
+#include <iostream>
+
+template<typename Type>
+Type min(Type a, Type b) {
+    if (a < b) {
+        return a;
+    }
+    return b;
+};
+
+template<typename Type>
+Type max(Type a, Type b) {
+    if (a > b) {
+        return a;
+    }
+    return b;
+};
+
+template<typename Type>
+void swap(Type &a, Type &b) {
+    Type temp = a;
+    a = b;
+    b = temp;
+};
+
+#endif
---
+++ module07/ex01/iter.hpp
@@ -0,0 +1,8 @@
+#ifndef ITER_HPP
+# define ITER_HPP
+
+template<typename T>
+void iter(T &arr, int length, toPrint(T element))
+
+
+#endif
